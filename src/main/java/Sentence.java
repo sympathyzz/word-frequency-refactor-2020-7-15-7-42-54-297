b8f134134aecc292123d82e4ec9d72137086eea1
96197 <961977096@qq.com>
2020-07-27 22:05:26 +0800

    [Gavin] refactor:change class Input to Sentence

--- src/main/java/Sentence.java
+++ src/main/java/Sentence.java
@@ -1,8 +1,8 @@
-public class Input {
+public class Sentence {
     private String value;
     private int count;
 
-    public Input(String w, int i) {
+    public Sentence(String w, int i) {
         this.value = w;
         this.count = i;
     }
