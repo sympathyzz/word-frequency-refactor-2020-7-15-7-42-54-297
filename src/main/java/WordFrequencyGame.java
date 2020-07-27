bc92df3b353d5ed8dab27b664ed6cd7d23710044
GAOGA4 <961977096@qq.com>
2020-07-27 16:11:31 +0800

    [Gavin] refactory:change s to word

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -17,8 +17,8 @@ public class WordFrequencyGame {
                 String[] words = sentence.split(SPLIT_SYMBOL);
 
                 List<Input> inputList = new ArrayList<>();
-                for (String s : words) {
-                    Input input = new Input(s, 1);
+                for (String word : words) {
+                    Input input = new Input(word, 1);
                     inputList.add(input);
                 }
                 Map<String, List<Input>> map = getListMap(inputList);
